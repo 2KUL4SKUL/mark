3174d7b120b999154c451599aab89e780be77c27
a.m.student <minorbrosadam@gmail.com>
2021-01-20 10:17:48 -0600

    Initial Commit

---
+++ Shared/markcoolerApp.swift
@@ -0,0 +1,17 @@
+//
+//  markcoolerApp.swift
+//  Shared
+//
+//  Created by A.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct markcoolerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
